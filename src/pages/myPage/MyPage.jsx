3e7a7a10644ede5eb443c5bf7aa3ccf4949bf267
Jungki <andy960609@gmail.com>
2023-03-08 12:16:09 +0900

    edit

--- src/pages/myPage/MyPage.jsx
+++ src/pages/myPage/MyPage.jsx
@@ -11,14 +11,11 @@ const EditUser = () => {
 
   const Authorization = sessionStorage.getItem("accessToken");
   const fetchList = async () => {
-    const { data } = await axios.get(
-      `${process.env.REACT_APP_DOG}/users/mypage`,
-      {
-        headers: {
-          Authorization,
-        },
-      }
-    );
+    const { data } = await axios.get(`${process.env.REACT_APP_DOG}/users/mypage`, {
+      headers: {
+        Authorization,
+      },
+    });
     setUser(data);
     setDogs(data.myDogs);
   };
@@ -32,11 +29,12 @@ const EditUser = () => {
       <Container>
         <StTopGroup>
           <div className="div" style={{ fontSize: "20px", fontWeight: "bold" }}>
-            안녕하세요 {user?.username}님!
+            <div> 안녕하세요 {user?.username}님!</div>
+            <StSurvey href="https://forms.gle/Ud6S9QmUgCY8Pedx8" target="_blank">
+              설문조사
+            </StSurvey>
           </div>
-          <div className="address">
-            {user?.address.split(" ").slice(0, 3).join(" ")}
-          </div>
+          <div className="address">{user?.address.split(" ").slice(0, 3).join(" ")}</div>
           <div className="address">{user?.email}</div>
         </StTopGroup>
         <StImgGroup>
@@ -64,19 +62,41 @@ const Container = styled.div`
 
 const StTopGroup = styled.div`
   margin-top: 20px;
-  margin-left: -140px;
   .div {
-    margin-bottom: 20px;
+    margin-bottom: 15px;
+    display: flex;
   }
   .address {
     margin-bottom: 10px;
     font-size: 13px;
   }
+  .div2 {
+    /* padding-left: 100px; */
+  }
 `;
 
 const StImgGroup = styled.div`
-  margin-top: 20px;
-  margin-bottom: 40px;
+  margin-top: 17px;
+  margin-bottom: 8px;
   display: flex;
   justify-content: center;
+`;
+
+const StSurvey = styled.a`
+  display: flex;
+  // 요소들의 배열 위치
+  justify-content: center;
+  // 요소들을 컨테이너의 가운데로 정렬
+  align-items: center;
+  width: 100px;
+  height: 20px;
+  margin-left: 80px;
+  text-decoration: none;
+  background-color: #2f58ac;
+  color: white;
+  border-radius: 10px;
+  &:hover {
+    background-color: #eea400;
+    transition: 0.4s;
+  }
 `;
